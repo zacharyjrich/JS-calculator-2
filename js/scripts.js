cfce992a40408557c4b7a58f1ded1e52fe79d0cb
Alex Migdol and Zachary Richards <alex.migdol@gmail.com>
2016-06-01 18:56:10 -0700

    add form fields for calculator in html and back-end js

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
